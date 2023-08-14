a1efd52398d699d2c62daa14b5a768d2f01c2661
AkshayJadahv-1922 <aksjadhav35@gmail.com>
2023-08-14 21:52:56 +0530

    Lecture 20 - Renamed Display column in Genre to DisplayOrder

---
+++ CodingWiki_DataAccess/Migrations/20230814161908_UpdatedColumnNameInGenreTable.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodingWiki_DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedColumnNameInGenreTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Display",
+                table: "Genres",
+                newName: "DisplayOrder");
+
+            //Before .NET 6,After renaming -  it was deleting and creating new column.
+            //In that scenario we had to take care of existing data in column
+            //we can execute sql statment to update new column value from old one.
+            //migrationBuilder.Sql(Statement) is used in that scenario to execute statement
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DisplayOrder",
+                table: "Genres",
+                newName: "Display");
+        }
+    }
+}
